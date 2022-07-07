7148dbd665c69220fa9a48fabfc11d4b2d29ba3b
NikolayKaurov <nikolaykaurov@gmail.com>
2022-07-07 16:46:16 +0700

    installed postcss-minify

--- src/toxin-slider.ts
+++ src/toxin-slider.ts
@@ -1,5 +1,12 @@
 import $ from 'jquery';
 
 $.fn.toxinSlider = function toxinSlider(this: JQuery): JQuery {
-  return this.append('<span class="toxin-slider">toxin-slider</span>');
+  return this.append(`
+    <div class="toxin-slider">
+      <div class="toxin-slider__inner-wrapper">
+        <div class="toxin-slider__thumb" style="left: 100%;">
+        </div>
+      </div>
+    </div>
+  `);
 };
